d5260462fa4d2bc2ae6526c6091b282ccf45f713
cold-viking <vovashagapov@gmail.com>
2026-05-18 16:57:15 +0500

    Add part of solution iter1

---
+++ SpaceProj/Entity/Star.cs
@@ -0,0 +1,32 @@
+﻿namespace SpaceProj.Entity;
+
+public class Star : CosmoObject
+{
+    public double TemperatureInKelvin { get; set; }
+
+    public string Color { get; set; }
+
+    public Star(
+        string name,
+        double massInTons,
+        double speedKmPerSecond,
+        double ageInBillionYears,
+        double temperatureInKelvin,
+        string color
+    ) : base(name, massInTons, speedKmPerSecond, ageInBillionYears)
+    {
+        TemperatureInKelvin = temperatureInKelvin;
+        Color = color;
+    }
+
+    public override string GetInfo()
+    {
+        return $"It is info about STAR\n" +
+               $"Name: {Name}, " +
+               $"Mass in tons: {MassInTons}, " +
+               $"Speed km/s: {SpeedKmPerSecond}, " +
+               $"Age in billion years: {AgeInBillionYears}, " +
+               $"Temperature in Kelvin: {TemperatureInKelvin}, " +
+               $"Color: {Color}";
+    }
+}
---
+++ SpaceProj/Services/JsonSerialization.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using SpaceProj.Entity;
+
+namespace SpaceProj.Services;
+
+public static class JsonSerialization
+{
+    public static void Run()
+    {
+        Star star = new Star(
+            "Sun",
+            9999,
+            220,
+            4.5,
+            5800,
+            "Yellow"
+        );
+        
+        string json = JsonSerializer.Serialize(star);
+
+        File.WriteAllText("star.json", json);
+
+        string jsonFromFile = File.ReadAllText("star.json");
+
+        Star? restoredStar = JsonSerializer.Deserialize<Star>(jsonFromFile);
+
+        if (restoredStar == null)
+        {
+            Console.WriteLine("Object was not deserialize from json");
+            return;
+        }
+
+        Console.WriteLine("Object was  deserialize from json");
+        Console.WriteLine(restoredStar.GetInfo());
+    }
+}
